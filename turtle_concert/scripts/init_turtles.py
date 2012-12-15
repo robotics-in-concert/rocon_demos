1126935ea2a9e04b72144d11234f0951cdfd48bf
Daniel Stonier <d.stonier@gmail.com>
2012-12-15 23:31:18 +0900

    concert launcher and parts upgraded for groovy rocon.

--- turtle_concert/scripts/init_turtles.py
+++ turtle_concert/scripts/init_turtles.py
@@ -1,51 +1,27 @@
 #!/usr/bin/env python
-
-# Software License Agreement (BSD License)
 #
-# Copyright (c) 2012, Yujin Robot, Daniel Stonier
-# All rights reserved.
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_demo/turtle_concert/master/LICENSE
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#  notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#  copyright notice, this list of conditions and the following
-#  disclaimer in the documentation and/or other materials provided
-#  with the distribution.
-#  * Neither the name of Yujin Robot nor the names of its
-#  contributors may be used to endorse or promote products derived
-#  from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+##############################################################################
+# Imports
+##############################################################################
 
 # Simple script to clear the turtlesim display on startup.
 
-import roslib; roslib.load_manifest('turtle_concert')
+import roslib
+roslib.load_manifest('turtle_concert')
 import rospy
-from turtlesim.srv import *
+import turtlesim.srv as turtlesim_srvs
 
 rospy.init_node('clear_turtles')
 rospy.wait_for_service('kill')
-kill_turtle = rospy.ServiceProxy('kill',Kill)
-response=kill_turtle("turtle1")
+kill_turtle = rospy.ServiceProxy('kill', turtlesim_srvs.Kill)
+response = kill_turtle("turtle1")
 
 # Only because we can't get services publicly exposed yet.
 # The master sync can do it, but am not yet doing it
 rospy.wait_for_service('spawn')
-spawn_turtle = rospy.ServiceProxy('spawn',Spawn)
-response=spawn_turtle(5.4,6.4,0.0,"kobuki")
-response=spawn_turtle(5.4,4.4,0.3,"guimul")
+spawn_turtle = rospy.ServiceProxy('spawn', turtlesim_srvs.Spawn)
+response = spawn_turtle(5.4,6.4,0.0,"kobuki")
+response = spawn_turtle(5.4,4.4,0.3,"guimul")
