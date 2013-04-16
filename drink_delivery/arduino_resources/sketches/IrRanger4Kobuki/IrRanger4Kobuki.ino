f71821d6588cbdca0cdead8a02f650bf98338b53
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-04-16 16:58:21 +0900

    fixes array problem

--- drink_delivery/arduino_resources/sketches/IrRanger4Kobuki/IrRanger4Kobuki.ino
+++ drink_delivery/arduino_resources/sketches/IrRanger4Kobuki/IrRanger4Kobuki.ino
@@ -10,12 +10,12 @@
 #include <std_msgs/Int16MultiArray.h>
 
 ros::NodeHandle  nh;
-//arduino_ressources::Rangers rangers_msg;
-std_msgs::Int16MultiArray rangers_msg;
+kobuki_arduino_msgs::Rangers rangers_msg;
 ros::Publisher pub_range( "rangers_data", &rangers_msg);
 
 const int nr_of_irs = 11;
 const int ir_pins[nr_of_irs] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int range_storage[nr_of_irs];
 unsigned long last_rangers_time, rangers_timing, last_blink_time, blink_timing, now;
 boolean led_on;
 int val;
@@ -25,12 +25,12 @@ void setup()
   nh.initNode();
   nh.advertise(pub_range);
   
-//  rangers_msg.header.frame_id = "kobuki_rangers";
-//  rangers_msg.ranges_length = nr_of_irs;
-  rangers_msg.data_length = nr_of_irs;
-  rangers_timing = 500; // ms
+  rangers_msg.header.frame_id = "kobuki_rangers";
+  rangers_msg.ranges_length = nr_of_irs;
+  rangers_msg.ranges = range_storage;
+  rangers_timing = 50; // publish the range value every 50 milliseconds, since it takes that long for the sensor to stabilize
   last_rangers_time = 0;
-  blink_timing = 500; // ms
+  blink_timing = 500; // blinking frequency in ms
   last_blink_time = 0;
   pinMode(13, OUTPUT);
   led_on = false;
@@ -38,24 +38,19 @@ void setup()
 
 void loop()
 {
-  // publish the range value every 50 milliseconds,
-  // since it takes that long for the sensor to stabilize
   now = millis();
   if ((now - last_rangers_time) > rangers_timing)
   {
     last_rangers_time = now;
-//    rangers_msg.header.stamp = nh.now();
+    rangers_msg.header.stamp = nh.now();
     for (unsigned int ir = 0; ir < nr_of_irs; ++ir)
     {
-      rangers_msg.data[ir] = analogRead(ir_pins[ir]);
-//      rangers_msg.data[ir] = 123;
-//      rangers_msg.ranges[ir] = 123;
-
+      rangers_msg.ranges[ir] = analogRead(ir_pins[ir]);
     }   
     pub_range.publish(&rangers_msg);
+    nh.spinOnce();
   }
-  nh.spinOnce();
-    
+   
   // Alive signal
   if ((now - last_blink_time) > blink_timing)
   {
@@ -72,4 +67,3 @@ void loop()
     }
   }
 }
-
