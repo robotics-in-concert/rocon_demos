4aa9469b0e54f829a344df49d52d2baa3cf63ca0
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-12-17 17:20:19 +0900

    adds vending machine package

---
+++ vending_machine_control/src/rocon_vending_machine/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/concert_apps/license/LICENSE
+#
+##############################################################################
+# Imports
+##############################################################################
+
+from .vending_machine import VendingMachine
---
+++ vending_machine_control/src/rocon_vending_machine/vending_machine.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_app_platform/license/LICENSE
+#
+##############################################################################
+# Imports
+##############################################################################
+
+import rospy
+
+class VendingMachine(object):
+    """
+        ROCON vending machine
+    """
+
+    def spin(self):
+        rospy.spin()
+
