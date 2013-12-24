19e1359920e09c2184642e8be809584d264434f9
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-12-24 20:03:30 +0900

    updates to first complete pipeline working version * renames rocon_vending_machine to vending_machine_control * fixes the VM manager * adds Arduino firmware (low-level control)

--- vending_machine_control/setup.py
+++ vending_machine_control/setup.py
@@ -3,9 +3,8 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=['rocon_vending_machine'],
+d = generate_distutils_setup(packages=['vending_machine_control'],
                              package_dir={'': 'src'},
-#                             scripts=[],
                              requires=['rospy'])
 
 setup(**d)
---
+++ vending_machine_control/src/vending_machine_control/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/concert_apps/license/LICENSE
+#
+
+from .vending_machine import VendingMachine
