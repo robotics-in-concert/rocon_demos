67e09a9c9e117773c89bdfd0c584950c36dbf01c
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-12-26 21:51:52 +0900

    adds the vending_machine_ar_client package

---
+++ vending_machine_ar_client/src/vending_machine_ar_client/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_demos/license/LICENSE
+#
+
+from .vending_machine_ar_client import VendingMachineARClient
