34b20912f330dcfe289ec60da2418e0ed34a5b52
Jihoon Lee <jihoonlee.in@gmail.com>
2013-12-29 21:24:58 +0900

    reduce sleep

--- dummy_ces_tablet/scripts/dummy_green_button.py
+++ dummy_ces_tablet/scripts/dummy_green_button.py
@@ -12,16 +12,16 @@ if __name__ == '__main__':
     rospy.init_node('dummy_buttons')
 
     input_pub = rospy.Publisher('/mobile_base/events/digital_input', kobuki_msgs.msg.DigitalInputEvent)
-    rospy.sleep(1)
+    rospy.sleep(0.5)
 
     m = kobuki_msgs.msg.DigitalInputEvent()
     rospy.loginfo("Pressing Green button")
     m.values = [False, True, False, False]
     input_pub.publish(m)
-    rospy.sleep(1)
+    rospy.sleep(0.5)
     rospy.loginfo("Releasing Green button")
     m.values = [True, True, False, False]
     input_pub.publish(m)
-    rospy.sleep(1)
+    rospy.sleep(0.5)
 
     rospy.loginfo("Done")
