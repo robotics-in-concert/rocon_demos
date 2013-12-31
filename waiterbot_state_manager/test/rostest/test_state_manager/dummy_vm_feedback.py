6e93e97c74e79f3f06162e470a3396e3a31af721
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-12-31 18:54:06 +0900

    waiterbot_state_manager: improves dummy_vm_feedback node

--- waiterbot_state_manager/test/rostest/test_state_manager/dummy_vm_feedback.py
+++ waiterbot_state_manager/test/rostest/test_state_manager/dummy_vm_feedback.py
@@ -13,32 +13,37 @@ class DummyVMFeedback(object):
     """
 
     def __init__(self):
-        # Vending machine feedback processor communication
+        # vending machine feedback processor communication
         self._sub_vm_feedback_enable = rospy.Subscriber('vm_feedback_proc/enable', std_msgs.Bool,
                                                         self._vmFeedbackEnableCB)
         self._processing_enabled = False
         self._pub_vm_feedback_result = rospy.Publisher('vm_feedback_proc/result', std_msgs.Bool, latch=True)
-        self._recognition_time = rospy.Duration(rospy.get_param("recognition_time", 1.0))
-        self._recognition_start = rospy.Time.now()
+        # subscribe to the order message (aka AR marker) in order to know when to start the recognition
+        self._sub_vm_feedback_enable = rospy.Subscriber('android_ui/drink_ar', std_msgs.UInt16, self._drinkARCB)
+        self._recognition_time = rospy.Duration(rospy.get_param("recognition_time", 10.0))
+        self._order_time = rospy.Time()
+        self._new_order = False
 
     def _vmFeedbackEnableCB(self, msg):
         self._processing_enabled = msg.data
         if self._processing_enabled:
-            self._recognition_start = rospy.Time.now()
             rospy.loginfo("DummyVMFeedback: Signal processing enabled.")
         else:
             rospy.loginfo("DummyVMFeedback: Signal processing disabled.")
+
+    def _drinkARCB(self, msg):
+        self._new_order = True
+        self._order_time = rospy.Time.now()
 
     def spin(self):
         while not rospy.is_shutdown():
             if self._processing_enabled:
                 rospy.loginfo("DummyVMFeedback: Processing light signal.")
                 msg = std_msgs.Bool()
-                if (rospy.Time.now() - self._recognition_start) > self._recognition_time:
+                if self._new_order and (rospy.Time.now() - self._order_time) > self._recognition_time:
                     rospy.loginfo("DummyVMFeedback: Light signal recognised!")
                     msg.data = True
-                     # reset time to allow publishing continuous successful signal recognitions
-                    self._recognition_start = rospy.Time.now()
+                    self._new_order = False
                 else:
                     msg.data = False
                 self._pub_vm_feedback_result.publish(msg)
