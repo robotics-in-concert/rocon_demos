2c7e07d9181c651e508f02b41ebfb2a7c5cb10bf
Daniel Stonier <d.stonier@gmail.com>
2014-01-02 12:31:55 +0900

    args and parameterisations for ease of use.

--- colour_signal/Readme.md
+++ colour_signal/Readme.md
@@ -1,4 +1,27 @@
-## History
+## Colour Signal
+
+### Parameters
+
+**difference_threshold** : used to set a threshold when comparing the difference between the selected hue and other hues
+(e.g. green vs blue and green vs red). If this is exceeded, the light signal is considered detected [default: 30]
+
+**frames_in_image_window** : the node collects this many images and only acts if the signal state in all agree 
+(makes it robust against getting the odd dirty image) [default: 5].
+
+**selected_hue** : can be "blue", "green" or "red". Green is usually the safest as there is not much 
+green in indoor environments, but if you should happen to have a purely green wall behind the led, then
+switch it [default: "green"].
+
+### Launchers
+
+ * **Nodes**
+  * `standalone.launch` : the official node - hook up to an sensor_msgs/Image producer. 
+ * **Test Launchers** 
+  * `bgr_histograms.launch` : calculate and plot bgr histograms for the test images.
+  * `hsv_histograms.launch` : calculate and plot hsv histograms for the test images. 
+  * `hues.launch` : calculates and displays the percentage of bgr hues in the reference images.
+  * `debug.launch` : runs the actual node with a fake publisher that switches the signal image every few seconds.
+
 
 
 ## Alternative Possibles
--- colour_signal/src/node.cpp
+++ colour_signal/src/node.cpp
@@ -41,6 +41,8 @@ public:
     **********************/
     ros::param::param<int>("~difference_threshold", _param_difference_threshold, 30);
     ros::param::param<int>("~frames_in_image_window", _param_frames_in_image_window, 5);  // number of frames to parse before making a decision (robustify against bad images).
+    ros::param::param<std::string>("~selected_hue", _param_selected_hue, "green");  // hue to select for detecting the signal state.
+    ros::param::param<std::string>("~image_topic", _param_image_topic, "image");
 
     /*********************
     ** Parameters
@@ -81,6 +83,26 @@ private:
     }
     return false;
   }
+
+  /**
+   * Accepts the b, g, r hue percentages and calculates
+   * a percentage difference between the selected hue and
+   * the others.
+   * @param hues
+   * @return true if threshold is exceeded (signal accepted)
+   */
+  bool _isSignalDetected(const std::vector<float>& hues) {
+    float average_difference;
+    if (_param_selected_hue == "blue") {
+      average_difference = (2*hues.at(0) - hues.at(1) - hues.at(2))*100.0/2.0;
+    } else if(_param_selected_hue == "green") {
+      average_difference = (2*hues.at(1) - hues.at(0) - hues.at(2))*100.0/2.0;
+    } else {
+      average_difference = (2*hues.at(2) - hues.at(0) - hues.at(1))*100.0/2.0;
+    }
+    return average_difference > _param_difference_threshold;
+  }
+
   void imageCallback(const sensor_msgs::Image::ConstPtr &msg) {
     if ( !_enabled ) {
       return;
@@ -100,8 +122,8 @@ private:
     bool convert_to_hsv = true;
     colour_signal::ColourImage image(cv_ptr->image, convert_to_hsv);
     std::vector<float> hues = colour_signal::spliceLightSignal(image);
-    float average_green_difference = (2*hues.at(1) - hues.at(0) - hues.at(2))*100.0/2.0;
-    if (_updateSignalState(average_green_difference > _param_difference_threshold)) { // signal state changed
+    bool signal_detected =_isSignalDetected(hues);
+    if (_updateSignalState(signal_detected)) { // signal state changed
       if(_signalled) {
         ROS_INFO_STREAM("ColourSignal : signal detected");
       } else {
@@ -118,7 +140,7 @@ private:
     if (msg->data) {
       ROS_INFO_STREAM("ColourSignal : enabled");
       ros::NodeHandle private_node_handle("~");
-      _image_subscriber = private_node_handle.subscribe("image", 10, &Jagi::imageCallback, this);
+      _image_subscriber = private_node_handle.subscribe(_param_image_topic, 10, &Jagi::imageCallback, this);
       _enabled = true;
     } else {
       ROS_INFO_STREAM("ColourSignal : disabled");
@@ -128,6 +150,8 @@ private:
   }
   int _param_difference_threshold;
   int _param_frames_in_image_window;
+  std::string _param_image_topic;
+  std::string _param_selected_hue;
   std::deque<bool> _recent_signals;
   bool _enabled;
   bool _signalled;
