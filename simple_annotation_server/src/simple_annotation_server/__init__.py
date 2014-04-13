c36bf07ff205b34e633d53807b1e67cad78ca331
Jihoon Lee <jihoonlee.in@gmail.com>
2014-04-13 12:20:40 +0900

    fix license url

--- simple_annotation_server/src/simple_annotation_server/__init__.py
+++ simple_annotation_server/src/simple_annotation_server/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # License: BSD
-#   https://raw.github.com/yujinrobot/yujin_ocs/license/LICENSE
+#   https://raw.github.com/robotics-in-concert/rocon_demos/license/LICENSE
 #
 
 from .annotation_server import SimpleAnnotationServer 
