0cdd12e428302abc2bb711e24f0342e3f547ef33
Jihoon Lee <jihoonlee.in@gmail.com>
2014-04-13 13:26:43 +0900

    test added

--- cafe_remote_order_manager/src/cafe_remote_order_manager/remote_order_manager.py
+++ cafe_remote_order_manager/src/cafe_remote_order_manager/remote_order_manager.py
@@ -6,11 +6,14 @@
 
 import rospy
 import cafe_msgs.msg as cafe_msgs
+import threading
 
 class RemoteOrderManager(object):
 
     def __init__(self): 
         self.orders = {}
+        self._init_rosapis()
+        self.lock = threading.Lock()
 
     def _init_rosapis(self):
         self.publisher = {}
@@ -22,11 +25,48 @@ class RemoteOrderManager(object):
         self.subscriber['remote_order_update'] = rospy.Subscriber('/cafe/remote_order_update', cafe_msgs.RemoteOrderUpdate, self.process_remote_order_update)
 
     def process_remote_order(self, msg):
-        pass
+        self.loginfo('Message Received')
+        
+        # TODO: it uses name as order unique id. If it receives an order with the same name, it just drops it. And no way to respond back
+        if msg.name in self.orders:
+            self.logwarn('order from [%s] is in preparation already. Dropping the new order..'%msg.name)
+            return
+        self.lock.acquire()
+        self.orders[msg.name] = msg
+        self.orders[msg.name].status = cafe_msgs.RemoteOrderStatus.ORDER_RECEIVED
+        self.update()
+        self.loginfo('Order from [%s] has been placed'%msg.name)
+        self.publisher['remote_order_status'].publish(self.orders[msg.name].status)
+        self.lock.release()
 
     def process_remote_order_update(self, msg):
-        pass
+        if not msg.name in self.orders:
+            self.logwarn('order from [%s] is not placed yet. Dropping the update..'%msg.name) 
+            return
+            
+        self.lock.acquire()
+        self.orders[msg.name].status = msg.status
+        self.orders[msg.name].estimated_arrival = msg.estimated_arrival
+        self.publisher['remote_order_status'].publish(self.orders[msg.name].status)
+
+        if msg.status == cafe_msgs.RemoteOrderStatus.ORDER_PICKED_UP:
+            self.loginfo('Order from [%s] has been picked up. Clearing..'%msg.name)
+            del self.orders[msg.name]
+
+        self.update()
+        self.lock.release()
+    
+    def update(self):
+        order_list = self.orders.values()
+        self.publisher['remote_order_list'].publish(order_list)
+
+    def spin(self):
+        # Publishes the empty latch topic
+        self.publisher['remote_order_list'].publish(cafe_msgs.RemoteOrderList())
+        rospy.spin()
 
     def loginfo(self, msg):
         rospy.loginfo('Remote Order Manager : ' + str(msg))
 
+    def logwarn(self, msg):
+        rospy.logwarn('Remote Order Manager : ' + str(msg))
