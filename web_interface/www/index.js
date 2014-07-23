674acaf9429fb0d0e032753da18dd335d49230aa
DongWook Lee <dwlee@yujinrobot.com>
2014-07-23 10:28:56 +0900

    create index css and add showing status bar

--- web_interface/www/index.js
+++ web_interface/www/index.js
@@ -3,6 +3,10 @@
     var order_pub_topic_type = "/cafe_msgs/OrderList";
     var remote_order_pub_topic_type = "/cafe_msgs/RemoteOrderList";
     var remote_order_update_pub_topic_type = "/cafe_msgs/RemoteOrderUpdate";
+    //test
+    var diagnostic_sub_topic_type = "diagnostic_msgs/DiagnosticArray";
+    var robot_status_type = "";
+    
     
     /*
     var defaultUrL = ""
@@ -24,8 +28,10 @@
     if(remote_order_update_pub_topic in rocon_interactions.remappings)
     	remote_order_update_pub_topic = rocon_interactions.remappings[remote_order_update_pub_topic];
     */
+  
+    var diagnostic_sub_topic = "diagnostics_agg";
     var defaultUrL = "ws://localhost:9090"
-
+  
     var nav_div;
     var remote_nav_div;
     var orders_div;
@@ -37,7 +43,7 @@
     $().ready(function(e){
 
       // setting ros callbacks()
-      //showMainMenu(true);
+      showMainMenu(true);
       settingROSCallbacks();
       settingMainMenu();
       ros.connect(defaultUrL);
@@ -61,11 +67,18 @@
       ros.on('connection',function() {
         console.log("Connected");
         // subscribe to order list
+        
         delive_order_client = new ROSLIB.ActionClient({
           ros : ros,
           serverName : '/kitchen_order',
           actionName : 'simple_delivery_msgs/DeliverOrderAction'
         });
+        var diagnostic_listener = new ROSLIB.Topic({
+          ros : ros,
+          name : diagnostic_sub_topic,
+          messageType: diagnostic_sub_topic_type
+          });
+        diagnostic_listener.subscribe(processDiagnostic);
 
       }
 
@@ -77,8 +90,14 @@
 
       ros.on('close',function() {
         console.log("Connection Close!");
+        //alert("ROS Connection Close!");
       }
       );
+    }
+
+    function processDiagnostic(data){
+      console.log("processDiagnostic");
+      console.log(data);
     }
 
     function settingMainMenu(){
@@ -110,7 +129,6 @@
       
         });
       };
-      
     };
     function showMainMenu(flag){
         if(flag === true){
@@ -123,279 +141,280 @@
         }
     }
 
-    function processRemoteOrderUpdate(msg){
-    	$('.'+msg.name).innerHTML = "<b>Name: </b>" + msg.name +
-	       "<br/><b>Estimated Time: </b>" + msg.estimated_arrival+" min." +  
-	       "<br/><b>Status : </b>" + STATUS_STRING[msg.status];
-    }
+
+//     function processRemoteOrderUpdate(msg){
+//     	$('.'+msg.name).innerHTML = "<b>Name: </b>" + msg.name +
+// 	       "<br/><b>Estimated Time: </b>" + msg.estimated_arrival+" min." +  
+// 	       "<br/><b>Status : </b>" + STATUS_STRING[msg.status];
+//     }
     
-    function processRemoteOrderList(msg) {
-    	var i;
-    	remote_nav_div.empty();
-    	remote_orders_div.empty();
-    	var rowdiv = document.createElement('div');
-       rowdiv.className = 'row-fluid';
+//     function processRemoteOrderList(msg) {
+//     	var i;
+//     	remote_nav_div.empty();
+//     	remote_orders_div.empty();
+//     	var rowdiv = document.createElement('div');
+//        rowdiv.className = 'row-fluid';
       
-    	for(i in msg.remote_orders) {
-    		// add into navigation bar
-    		var navli = createRemoteOrderLi(msg.remote_orders[i]); 
-    		remote_nav_div.append(navli);
+//     	for(i in msg.remote_orders) {
+//     		// add into navigation bar
+//     		var navli = createRemoteOrderLi(msg.remote_orders[i]); 
+//     		remote_nav_div.append(navli);
     		
-    		var div = createRemoteOrderDiv(msg.remote_orders[i]);
-           rowdiv.appendChild(div);
+//     		var div = createRemoteOrderDiv(msg.remote_orders[i]);
+//            rowdiv.appendChild(div);
     		
-           if((i+1) % 3 == 0){
-        	   remote_orders_div.append(rowdiv);
-        	   rowdiv = document.createElement('div');
-        	   rowdiv.className = 'row-fluid';
-        	   var hr = document.createElement('hr');
-        	   remote_orders_div.append(hr);
-             }
-    	}
-    	remote_orders_div.append(rowdiv);
+//            if((i+1) % 3 == 0){
+//         	   remote_orders_div.append(rowdiv);
+//         	   rowdiv = document.createElement('div');
+//         	   rowdiv.className = 'row-fluid';
+//         	   var hr = document.createElement('hr');
+//         	   remote_orders_div.append(hr);
+//              }
+//     	}
+//     	remote_orders_div.append(rowdiv);
 
-    }
+//     }
     
-    function createRemoteOrderLi(order){
-    	order.order_id = "Remote";
-    	order.table_id = "remote order";
-       var li = document.createElement('li');
-       var p = document.createElement('p');
-       p.className = order.name
-       p.innerHTML = "<b>Name: </b>" + order.name +
-				       "<br/><b>Estimated Time: </b>" + order.estimated_arrival+" min." +  
-				       "<br/><b>Status : </b>" + STATUS_STRING[order.status];
-       li.appendChild(p);
+//     function createRemoteOrderLi(order){
+//     	order.order_id = "Remote";
+//     	order.table_id = "remote order";
+//        var li = document.createElement('li');
+//        var p = document.createElement('p');
+//        p.className = order.name
+//        p.innerHTML = "<b>Name: </b>" + order.name +
+// 				       "<br/><b>Estimated Time: </b>" + order.estimated_arrival+" min." +  
+// 				       "<br/><b>Status : </b>" + STATUS_STRING[order.status];
+//        li.appendChild(p);
 
-       $(li).hover(
-            function() { this.style.background= "gray"; },
-            function() { this.style.background= "";     }
-        );
-       $(li).click(function() {
-         hero_div.empty();
-         var hero_element = createHeroDiv(order);
-         hero_div.append(hero_element);
-        });
+//        $(li).hover(
+//             function() { this.style.background= "gray"; },
+//             function() { this.style.background= "";     }
+//         );
+//        $(li).click(function() {
+//          hero_div.empty();
+//          var hero_element = createHeroDiv(order);
+//          hero_div.append(hero_element);
+//         });
 
-        return li;
-      }
-    function createRemoteOrderDiv(order){
-    	var div = document.createElement('div');
-        div.className = "span4";
-        div.style.border = "thin dotted";
-        div.style.padding = "5pt";
-        var h = document.createElement('h3');
-        h.innerHTML = "Order : " + order.order_id;
-        div.appendChild(h);
+//         return li;
+//       }
+//     function createRemoteOrderDiv(order){
+//     	var div = document.createElement('div');
+//         div.className = "span4";
+//         div.style.border = "thin dotted";
+//         div.style.padding = "5pt";
+//         var h = document.createElement('h3');
+//         h.innerHTML = "Order : " + order.order_id;
+//         div.appendChild(h);
 
-        var elements_robot= createRowLine('Name: ',order.name || "");
-        div.appendChild(elements_robot);
+//         var elements_robot= createRowLine('Name: ',order.name || "");
+//         div.appendChild(elements_robot);
 
-        var elements_table = createRowLine('Extimated Time: ' ,(order.estimated_arrival || "")+ " min.");
-        div.appendChild(elements_table);
+//         var elements_table = createRowLine('Extimated Time: ' ,(order.estimated_arrival || "")+ " min.");
+//         div.appendChild(elements_table);
 
-        var menudiv = createMenuDiv(order.menus);
-        div.appendChild(menudiv); 
+//         var menudiv = createMenuDiv(order.menus);
+//         div.appendChild(menudiv); 
 
-        $(div).hover(
-            function() { this.style.background= "gray"; },
-            function() { this.style.background= "";     }
-        );
+//         $(div).hover(
+//             function() { this.style.background= "gray"; },
+//             function() { this.style.background= "";     }
+//         );
 
-        $(div).click(function() {
-          hero_div.empty();
-          var hero_element = createHeroDiv(order);
-          hero_div.append(hero_element);
+//         $(div).click(function() {
+//           hero_div.empty();
+//           var hero_element = createHeroDiv(order);
+//           hero_div.append(hero_element);
 
-        });
+//         });
 
-        return div;
-    }
+//         return div;
+//     }
 
-    function processOrderList(msg) {
-      var i;
+//     function processOrderList(msg) {
+//       var i;
 
-      nav_div.empty();
-      hero_div.empty();
-      orders_div.empty();
+//       nav_div.empty();
+//       hero_div.empty();
+//       orders_div.empty();
 
-      var rowdiv = document.createElement('div');
-      rowdiv.className = 'row-fluid';
+//       var rowdiv = document.createElement('div');
+//       rowdiv.className = 'row-fluid';
 
-      // create Hero 
-      var hero_element = createHeroDiv(msg.orders[0]);
-      hero_div.append(hero_element);
+//       // create Hero 
+//       var hero_element = createHeroDiv(msg.orders[0]);
+//       hero_div.append(hero_element);
 
-      for(i in msg.orders) {
-        // add into navigation bar
-        var navli = createOrderLi(msg.orders[i]); 
-        nav_div.append(navli);
+//       for(i in msg.orders) {
+//         // add into navigation bar
+//         var navli = createOrderLi(msg.orders[i]); 
+//         nav_div.append(navli);
 
-        // add into order lists
-        var div = createOrderDiv(msg.orders[i]);
-        rowdiv.appendChild(div);
+//         // add into order lists
+//         var div = createOrderDiv(msg.orders[i]);
+//         rowdiv.appendChild(div);
 
-        // next row
-        if((i+1) % 3 == 0)
-        {
-          orders_div.append(rowdiv);
-          rowdiv = document.createElement('div');
-          rowdiv.className = 'row-fluid';
-          var hr = document.createElement('hr');
-          orders_div.append(hr);
-        }
-      }
-      orders_div.append(rowdiv);
-    }
+//         // next row
+//         if((i+1) % 3 == 0)
+//         {
+//           orders_div.append(rowdiv);
+//           rowdiv = document.createElement('div');
+//           rowdiv.className = 'row-fluid';
+//           var hr = document.createElement('hr');
+//           orders_div.append(hr);
+//         }
+//       }
+//       orders_div.append(rowdiv);
+//     }
 
-    function createHeroDiv(order)
-    {
-      var externaldiv = document.createElement('div');
+//     function createHeroDiv(order)
+//     {
+//       var externaldiv = document.createElement('div');
 
-      if(order == undefined)
-      {
-        externaldiv.innerHTML = "No Order";
-        return externaldiv;
-      }
+//       if(order == undefined)
+//       {
+//         externaldiv.innerHTML = "No Order";
+//         return externaldiv;
+//       }
 
-      var h1 = document.createElement('h2');
-      h1.innerHTML = "Order : " + (order.order_id || "");
-      externaldiv.appendChild(h1);
-      if(order.order_id === "Remote"){
-    	  var elements_robot= createRowLine('Name : ',order.name || "");
-    	  externaldiv.appendChild(elements_robot);
-    	  var elements_title = createRow('h5','row-fluid',['Estimated Time','Status'],'span6');
-    	  externaldiv.appendChild(elements_title);
+//       var h1 = document.createElement('h2');
+//       h1.innerHTML = "Order : " + (order.order_id || "");
+//       externaldiv.appendChild(h1);
+//       if(order.order_id === "Remote"){
+//     	  var elements_robot= createRowLine('Name : ',order.name || "");
+//     	  externaldiv.appendChild(elements_robot);
+//     	  var elements_title = createRow('h5','row-fluid',['Estimated Time','Status'],'span6');
+//     	  externaldiv.appendChild(elements_title);
 
-    	  var elements = createRow('div','row-fluid',[(order.estimated_arrival || "")+" min.",STATUS_STRING[order.status]],'span6');
-    	  externaldiv.appendChild(elements);
-       }
-      else{
-    	  var elements_robot= createRowLine('Robot : ',order.robot_name || "");
-          externaldiv.appendChild(elements_robot);
+//     	  var elements = createRow('div','row-fluid',[(order.estimated_arrival || "")+" min.",STATUS_STRING[order.status]],'span6');
+//     	  externaldiv.appendChild(elements);
+//        }
+//       else{
+//     	  var elements_robot= createRowLine('Robot : ',order.robot_name || "");
+//           externaldiv.appendChild(elements_robot);
 
-          var elements_title = createRow('h5','row-fluid',['Table','Status'],'span6');
-          externaldiv.appendChild(elements_title);
+//           var elements_title = createRow('h5','row-fluid',['Table','Status'],'span6');
+//           externaldiv.appendChild(elements_title);
 
-          var elements = createRow('div','row-fluid',[order.table_id || "" ,STATUS_STRING[order.status]],'span6');
-          externaldiv.appendChild(elements);
-      }
+//           var elements = createRow('div','row-fluid',[order.table_id || "" ,STATUS_STRING[order.status]],'span6');
+//           externaldiv.appendChild(elements);
+//       }
 
-      var menudiv = createMenuDiv(order.menus);
-      externaldiv.appendChild(menudiv); 
+//       var menudiv = createMenuDiv(order.menus);
+//       externaldiv.appendChild(menudiv); 
             
-      return externaldiv;
-    }
+//       return externaldiv;
+//     }
 
-    function createOrderLi(order) {
-      var li = document.createElement('li');
-      var p = document.createElement('p');
-      p.innerHTML = "#" + order.order_id + 
-      				   "<br/><b>Table : </b>" + order.table_id + 
-      				   "<br/><b>Robot : </b>" + order.robot_name +  
-      				   "<br/> Status : </b>" + STATUS_STRING[order.status];
-      li.appendChild(p);
+//     function createOrderLi(order) {
+//       var li = document.createElement('li');
+//       var p = document.createElement('p');
+//       p.innerHTML = "#" + order.order_id + 
+//       				   "<br/><b>Table : </b>" + order.table_id + 
+//       				   "<br/><b>Robot : </b>" + order.robot_name +  
+//       				   "<br/> Status : </b>" + STATUS_STRING[order.status];
+//       li.appendChild(p);
 
-      $(li).hover(
-          function() { this.style.background= "gray"; },
-          function() { this.style.background= "";     }
-      );
-      $(li).click(function() {
-        hero_div.empty();
-        var hero_element = createHeroDiv(order);
-        hero_div.append(hero_element);
-      });
+//       $(li).hover(
+//           function() { this.style.background= "gray"; },
+//           function() { this.style.background= "";     }
+//       );
+//       $(li).click(function() {
+//         hero_div.empty();
+//         var hero_element = createHeroDiv(order);
+//         hero_div.append(hero_element);
+//       });
 
-      return li;
-    }
+//       return li;
+//     }
 
-    function createOrderDiv(order) {
-      var div = document.createElement('div');
-      div.className = "span4";
-      div.style.border = "thin dotted";
-      div.style.padding = "5pt";
-      var h = document.createElement('h3');
-      h.innerHTML = "Order : " + order.order_id;
-      div.appendChild(h);
+//     function createOrderDiv(order) {
+//       var div = document.createElement('div');
+//       div.className = "span4";
+//       div.style.border = "thin dotted";
+//       div.style.padding = "5pt";
+//       var h = document.createElement('h3');
+//       h.innerHTML = "Order : " + order.order_id;
+//       div.appendChild(h);
 
-      var elements_robot= createRowLine('Robot : ',order.robot_name | "");
-      div.appendChild(elements_robot);
+//       var elements_robot= createRowLine('Robot : ',order.robot_name | "");
+//       div.appendChild(elements_robot);
 
-      var elements_table = createRowLine('Table : ' ,order.table_id | "");
-      div.appendChild(elements_table);
+//       var elements_table = createRowLine('Table : ' ,order.table_id | "");
+//       div.appendChild(elements_table);
 
-      var elements_status = createRowLine('Status : ',STATUS_STRING[order.status] | "");
-      div.appendChild(elements_status);
+//       var elements_status = createRowLine('Status : ',STATUS_STRING[order.status] | "");
+//       div.appendChild(elements_status);
 
-      var menudiv = createMenuDiv(order.menus);
-      div.appendChild(menudiv); 
+//       var menudiv = createMenuDiv(order.menus);
+//       div.appendChild(menudiv); 
 
-      $(div).hover(
-          function() { this.style.background= "gray"; },
-          function() { this.style.background= "";     }
-      );
+//       $(div).hover(
+//           function() { this.style.background= "gray"; },
+//           function() { this.style.background= "";     }
+//       );
 
-      $(div).click(function() {
-        hero_div.empty();
-        var hero_element = createHeroDiv(order);
-        hero_div.append(hero_element);
+//       $(div).click(function() {
+//         hero_div.empty();
+//         var hero_element = createHeroDiv(order);
+//         hero_div.append(hero_element);
 
-      });
+//       });
 
-      return div;
-    }
+//       return div;
+//     }
 
-    function createRowLine(name,element)
-    {
-      var div = createDiv('','row-fluid');
-      var title = document.createElement('strong');
-      title.className = 'span4';
-      title.innerHTML = name; 
-      div.appendChild(title);
+//     function createRowLine(name,element)
+//     {
+//       var div = createDiv('','row-fluid');
+//       var title = document.createElement('strong');
+//       title.className = 'span4';
+//       title.innerHTML = name; 
+//       div.appendChild(title);
 
-      var el = createDiv(element,'span8');
-//      el.style.marginTop = '10px';
-      div.appendChild(el);
+//       var el = createDiv(element,'span8');
+// //      el.style.marginTop = '10px';
+//       div.appendChild(el);
 
-      return div;
-    }
+//       return div;
+//     }
 
 
-    function createMenuDiv(menus) 
-    {
-      var externaldiv = document.createElement('div');
-      var div = createRow('h5','row-fluid',['Name','Size','Qty'],'span4');
+//     function createMenuDiv(menus) 
+//     {
+//       var externaldiv = document.createElement('div');
+//       var div = createRow('h5','row-fluid',['Name','Size','Qty'],'span4');
 
-      externaldiv.appendChild(div);
+//       externaldiv.appendChild(div);
 
-      var i;
-      for(i in menus) {
-        var m = menus[i]; 
-        var d= createRow('div','row-fluid',[m.name,m.size,m.qty],'span4');
+//       var i;
+//       for(i in menus) {
+//         var m = menus[i]; 
+//         var d= createRow('div','row-fluid',[m.name,m.size,m.qty],'span4');
         
-        if (m.qty === 0)continue;
-        externaldiv.appendChild(d);
-      }
-      return externaldiv;
-    }
+//         if (m.qty === 0)continue;
+//         externaldiv.appendChild(d);
+//       }
+//       return externaldiv;
+//     }
 
-    function createRow(rowel,rowclass,elements,elementclass)
-    {
-      var div = document.createElement(rowel);
-      div.className = rowclass;
+//     function createRow(rowel,rowclass,elements,elementclass)
+//     {
+//       var div = document.createElement(rowel);
+//       div.className = rowclass;
 
-      var i;
-      for(i in elements) { 
-        var e = createDiv(elements[i],elementclass);
-        div.appendChild(e);
-      }
-      return div;
-    }
+//       var i;
+//       for(i in elements) { 
+//         var e = createDiv(elements[i],elementclass);
+//         div.appendChild(e);
+//       }
+//       return div;
+//     }
     
-    function createDiv(text,elementclass) {
-      var div = document.createElement('div'); 
-      div.className = elementclass;
-      div.innerHTML = text;
+//     function createDiv(text,elementclass) {
+//       var div = document.createElement('div'); 
+//       div.className = elementclass;
+//       div.innerHTML = text;
 
-      return div;
-    }
+//       return div;
+//     }
