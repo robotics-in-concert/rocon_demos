d0dfb44cbf3fe020fbd59bc750209e2dc23819e9
Jihoon Lee <jihoonlee.in@gmail.com>
2014-07-27 16:51:29 +0900

    add map_saver package for saving cropped map into filesystem using ros service handle

---
+++ map_saver/scripts/map_saver.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_demos/license/LICENSE
+#
+
+import rospy 
+import map_store.srv as map_store_srvs
+
+def process_save_map(req):
+    filename = rospy.get_param('filename', req.name) 
+    tmp_name = filename + '_tmp'
+
+    tmp_output = subprocess.check_output(['rosrun','map_server','map_saver','-f',tmp_name])
+    rospy.sleep(2.0)
+    tmp_name = tmp_name + '.yaml'
+    crop_output = subprocess.check_output(['rosrun','map_server','crop_map',tmp_name,filename])
+    rospy.loginfo('Map Saved into %s'%str(filename))
+
+    return map_store_srvs.SaveMapResponse()
+    
+
+if __name__ == '__main__':
+    rospy.init_node('map_saver')
+
+    srv_saver = rospy.Service('/save_map', map_store_srvs.SaveMap, process_save_map)
+    rospy.spin()
