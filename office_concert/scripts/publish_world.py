8dd0ff3e6b6a70f291d3577c423b3c1a4ad45a69
Jihoon Lee <jihoonlee.in@gmail.com>
2014-10-10 19:31:18 +0900

    updates

--- office_concert/scripts/publish_world.py
+++ office_concert/scripts/publish_world.py
@@ -13,24 +13,39 @@ def publish_map(world, namespace):
     map_ac.loadData()
 
     # Publish the map on server side; topic type is get from the annotation info
-    map_ac.publish(map_topic, None, True, False)   # i.e. by_server=True, as_list=False
+    map_ac.publish(map_topic, None, False, False)   # i.e. by_server=True, as_list=False
+    
+    return map_ac
 
 def publish_location(world, namespace):
-    #TODO
-    pass
+    table_topic = rospy.get_param('table_topic')
+    viz_table_topic = rospy.get_param('viz_table_topic')
 
-def publish_ar_marker(world):
-    #TODO
-    pass
+    table_ac = world_canvas_client.AnnotationCollection(world=world, types=['yocs_msgs/Table'], srv_namespace=namespace)
+    table_ac.loadData()
+    table_ac.publish(table_topic, 'yocs_msgs/TableList', by_server=False, as_list=True)
+    table_ac.publishMarkers(viz_table_topic)
+    return table_ac
+
+
+def publish_ar_marker(world, namespace):
+    ar_topic = rospy.get_param('ar_topic')
+    viz_ar_topic = rospy.get_param('viz_ar_topic')
+
+    ar_ac = world_canvas_client.AnnotationCollection(world=world, types=['ar_track_alvar_msgs/AlvarMarker'], srv_namespace=namespace)
+    ar_ac.loadData()
+#    ar_ac.publish(ar_topic, 'ar_track_alvar_msgs/AlvarMarkers', by_server=False, as_list=True)
+#    ar_ac.publishMarkers(viz_ar_topic)
+    return ar_ac
+
 
 if __name__ == '__main__':
     rospy.init_node('world_canvas_client')
     world = rospy.get_param('world')
     namespace = rospy.get_param('wc_namespace')
 
-    publish_map(world, namespace)
-
-    #publish_location(world, namespace)
-    #publish_ar_marker(world, namespace)
+    map_ac = publish_map(world, namespace)
+    table_ac = publish_location(world, namespace)
+    ar_ac = publish_ar_marker(world, namespace)
     rospy.loginfo("Done")
     rospy.spin()
