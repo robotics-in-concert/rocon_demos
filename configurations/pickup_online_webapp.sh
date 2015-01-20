575e30c68ccec8c41146db9a7ff8522326b76653
dwlee <dwlee@yujinrobot.com>
2015-01-20 19:11:17 +0900

    update for setting pickup delivery environments

--- configurations/pickup_online_webapp.sh
+++ configurations/pickup_online_webapp.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 export CONCERT_AUTO_ENABLE_SERVICES="[admin, pickup_delivery]"
+export CONCERT_AUTO_ENABLE_WORKFLOWS="--web --engine --workflow pickup_delivery_wf"
 #export CONCERT_WEBSERVER_ADDRESS="webapp.robotconcert.org"
 export CONCERT_WEBSERVER_ADDRESS="toyweb.cafe24.com:3008"
+
