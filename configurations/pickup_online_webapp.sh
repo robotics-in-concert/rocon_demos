1b46384e49a19943f9ab79bc2c4d677e69f25fbb
Jihoon Lee <jihoonlee.in@gmail.com>
2015-01-20 15:50:53 +0900

    add pickup delivery configuration

---
+++ configurations/pickup_online_webapp.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+export CONCERT_AUTO_ENABLE_SERVICES="[admin, pickup_delivery]"
+#export CONCERT_WEBSERVER_ADDRESS="webapp.robotconcert.org"
+export CONCERT_WEBSERVER_ADDRESS="toyweb.cafe24.com:3008"
