0a587905ad1504b82d7c570fef8cde55a8db362c
Hughie <hrkim@yujinrobot.com>
2015-10-27 16:21:46 +0900

    roboworld2015 cafe_concert

--- configurations/pickup_online_webapp.sh
+++ configurations/pickup_online_webapp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-export CONCERT_AUTO_ENABLE_SERVICES="[admin, pickup_delivery]"
+export CONCERT_AUTO_ENABLE_SERVICES="[admin, cafe_delivery]"
 export CONCERT_WEBSERVER_ADDRESS="webapp.robotconcert.org"
